278a61f4b8431bacea61d19be91107e369133508
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-07-28 19:20:57 +0530

    router functionality updated
    clean up code and added routers separately for admin and shop

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require("express");
 const app = express();
 const port =  4500;
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+
+const userRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use('/add-product',(req, res, next)=>{
-    res.send('<!DOCTYPE html><html><head><title>My Page</title></head><body><form action="/product" method="POST"><input type="text" name="title"><br><br><input type="text" name="page"><br><br><button type="submit">Add product</button></form></body></html>');
-})
-app.use('/product',(req, res, next)=>{
-    console.log(req.body);
-    res.redirect("/");
-})
-app.use('/',(req, res, next)=>{
-    res.send("hello from home page");
-})
+app.use(adminRoutes);
+
+app.use(userRoutes);
+app.use((req, res, next)=>{
+    res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(port);
