7d9fd2f2def392e4c145cd7f25ea3aa4c4e4b60a
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-07-28 18:48:12 +0530

    body parser added
    there's a form which has two inputs when user submit it shows their value in console

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const port =  4500;
+const bodyParser = require('body-parser');
 
-app.use((req, res, next)=>{
-    console.log("in the first middleware");
-    next();
-})
-app.use((req, res, next)=>{
-    res.send("in the second middleware");
-})
+app.use(bodyParser.urlencoded({extended:true}));
 
+app.use('/add-product',(req, res, next)=>{
+    res.send('<!DOCTYPE html><html><head><title>My Page</title></head><body><form action="/product" method="POST"><input type="text" name="title"><br><br><input type="text" name="page"><br><br><button type="submit">Add product</button></form></body></html>');
+})
+app.use('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect("/");
+})
+app.use('/',(req, res, next)=>{
+    res.send("hello from home page");
+})
 
 app.listen(port);
